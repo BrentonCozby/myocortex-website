e37215ed74de6f21751da0f524f1aec34b44ab15
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-16 15:06:25 -0400

    add backup email

--- src/mail/contact_me.php
+++ src/mail/contact_me.php
@@ -7,6 +7,7 @@
 
     // Create the email and send the message
     $to = 'info@myocortex.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $backup = 'brenton.cozby@gmail.com';
     $email_subject = "Myocortex.com Contact Form:  $subject";
     $email_body = "Subject: $subject\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: contact@myocortex.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
@@ -16,6 +17,12 @@
     }
     else {
         echo "mailed failed to send to: $to";
+    }
+    if(mail($to,$email_subject,$email_body,$headers)) {
+        echo "mail sent successfully to: $backup";
+    }
+    else {
+        echo "\n\nmailed failed to send to: $to";
     }
     return true;
 ?>
