9e60d9cd4f41d7d2e9bda0dfa39b820ddc16b331
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-16 14:58:23 -0400

    change email

--- src/mail/contact_me.php
+++ src/mail/contact_me.php
@@ -6,7 +6,7 @@
     $message = strip_tags(htmlspecialchars($_POST['message']));
 
     // Create the email and send the message
-    $to = 'Courtney@myocortex.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'info@myocortex.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Myocortex.com Contact Form:  $subject";
     $email_body = "Subject: $subject\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: contact@myocortex.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
